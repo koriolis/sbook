7400183a501a5f46cfef3cd198d57da59af4413c
Nuno Ferreira <nuno.ferreira@wiz.pt>
2012-01-26 12:19:46 +0000

    Changed sample-config file to reflect the latest configs used.
    Each time sbook runs it creates a config file for the domain used based
    on the sample-config template. This way it's one chore less to remember
    the latest settings we use.

--- www/sbook/config/sample_config.php
+++ www/sbook/config/sample_config.php
@@ -4,19 +4,44 @@
 define('CONFIG',true);
 
 // database config
-define('DATABASE_TYPE','mysql');
-define('DATABASE_HOST','localhost');
-define('DATABASE_NAME','db_name');
-define('DATABASE_prefix','');
-define('DATABASE_USERNAME','db_username');
-define('DATABASE_PASSWORD','db=password');
+define('DEFAULT_DSN','mysql://user:password@host/dbname');
 
-// General config options
-define('BASEURI','');
+define('BASEURI','http://sbook/');
+
+define('TEMPLATES',APP.'templates/');
+define('UPLOADS',APP.'uploads/');
+
+define('TEMPLATES_URI',BASEURI.'app/templates/');
+define('JS_URI',TEMPLATES_URI.'js/');
+define('CSS_URI',TEMPLATES_URI.'css/');
+define('IMG_URI',TEMPLATES_URI.'img/');
+define('UPLOADS_URI',BASEURI.'app/uploads/');
+
+define('BO_TEMPLATES',APP.'templates/backoffice/');
+define('BO_URI',BASEURI.'backoffice/');
+define('BO_TEMPLATES_URI',BASEURI.'app/templates/backoffice/');
+define('BO_JS_URI',BO_TEMPLATES_URI.'js/');
+define('BO_CSS_URI',BO_TEMPLATES_URI.'css/');
+define('BO_IMG_URI',BO_TEMPLATES_URI.'img/');
+
+
+//Swtch between show 404 page (true) or sBook default behaviour
+define('SHOW404',true);
+define('PAGE404_URI',BASEURI.'404.html');
 
 // General error options
 define('ERROR_DEBUG','1');
 define('ERROR_PHP','');
-define('ERROR_MAIL','admin@some.site.com');
+define('ERROR_MAIL','nuno.ferreira@wiz.pt');
 
+// Access Control
+//
+define('ACCESS_RESTRICTED',0);
+define('ACCESS_USER','user');
+define('ACCESS_PASS','password');
+
+//Email
+define('MAIL_SERVER','mail.wiz.pt');
+define('MAIL_FROM_EMAIL','info@wiz.pt');
+define('MAIL_FROM_NAME','Wiz Interactive');
 ?>
